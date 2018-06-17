ba7f997818a1d5ad2779e682234f0111fb41c43e
Alexander Figul <alexander.figul@melsicon.de>
2018-06-17 17:50:57 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/annika', function (req, res) {
+  res.send('Hello Annika');
+});
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3000!');
+});
