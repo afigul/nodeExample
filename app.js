7a9c9e7a816497be4011438b55509ba5bdb70e7b
Alexander Figul <alexander.figul@melsicon.de>
2018-06-17 18:50:01 +0200

    json

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.get('/:name', function (req, res) {
+app.get('/hello/:name', function (req, res) {
   var name = req.params.name;  
   res.send('Hello ' +  name + '!');
 });
+
+app.get('/json', function (req, res) {
+  var json = {"foo": "bar"};
+  res.json(json);
+});
+
 
 app.listen(3001, function () {
   console.log('Example app listening on port 3001!');
